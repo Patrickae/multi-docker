9630df41813ab4e3d499b1f45488112daaf0f31a
Andrew Patrick <andyepatrick@gmail.com>
2018-11-28 14:47:38 -0500

    comit

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <Link to='/'>Home</Link>
-            <Link to='/otherpage'>Other Page</Link>
+            <Link to='/otherpage'>Some Other Page</Link>
           </header>
           <Route exact path="/" component={Fib} />
           <Route path='/otherpage' component={OtherPage} />
